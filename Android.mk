4705ea608cb4d41ffdc578929bfd271334fc21c6
wuzhentang <wuzhentang@hotmail.com>
2019-01-25 10:51:43 +0800

    fix: static link libcrypto instead of dynamic link which may cause system boot fail in marshmallow

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 define all-c-files-under
-	$(patsubst ./%,%, $(shell find $(LOCAL_PATH) -name  "examples" -prune -o  -name "*.c" \
+$(patsubst ./%,%, $(shell find $(LOCAL_PATH) -name  "examples" -prune -o  -name "*.c" \
 		-and -not -name ".*" \
 		-and -not -name "t_timer.c"  \
 		-and -not -name "t_api.c" \
@@ -14,24 +14,25 @@ endef
 define all-subdir-c-files
 	$(call all-c-files-under,.)
 endef
-			   
-#C_FILES := $(filter-out "$(LOCAL_PATH)/src/t_timer.c" "$(LOCAL_PATH)/src/t_api.c" "$(LOCAL_PATH)/src/t_uuid.c" , $(call all-subdir-c-files))
+	   
 C_FILES := $(call all-subdir-c-files)
 
-#$(warning $(C_FILES)
+#$(warning $(C_FILES))
   
 LOCAL_SRC_FILES := $(C_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES += \
-			$(LOCAL_PATH) \
-			$(LOCAL_PATH)/include
+	$(LOCAL_PATH)/include
 
 LOCAL_CFLAGS += -O2 
 LOCAL_CFLAGS += -DHAVE_CONFIG_H 
 LOCAL_CFLAGS += -Iexternal/openssl/include
-LOCAL_SHARED_LIBRARIES := libc libm libcutils libnetutils libcrypto 
+LOCAL_SHARED_LIBRARIES := libc libm libcutils libnetutils
+LOCAL_STATIC_LIBRARIES := libcrypto_static
 
-LOCAL_MODULE := iperf 
+
+LOCAL_MODULE := iperf3 
 LOCAL_MODULE_TAGS := debug 
 
 include $(BUILD_EXECUTABLE) 
+
